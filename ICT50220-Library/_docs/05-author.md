91bcb45543c3da3c6721438775d1d38c030a2968
Aleesha Bretnall <aleeshabretnall@gmail.com>
2023-03-14 15:08:46 +0800

    Author - migration, model, etc stubs

---
+++ ICT50220-Library/_docs/05-author.md
@@ -0,0 +1,11 @@
+# Author
+
+## Table Design
+
+| Field Name | Type    | Size | Options                 | Default |
+|------------|---------|------|-------------------------|---------|
+| id         | big int | -    | unsigned, autoincrement |         |
+| first name | string  | 128  |                         |         |
+| last name  | string  | 128  | nullable                |         |
+| country    | string  | 128  | nullable                |         |
+| dob        | date    | -    | nullable                |         |
---
+++ ICT50220-Library/database/migrations/2023_03_14_070607_create_authors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 128);
+            $table->string('last_name', 128)->nullable();
+            $table->string('country', 128)->nullable();
+            $table->date('dob')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authors');
+    }
+};
